5b5f07d6ff7fca0e0ec829aa561b7d0373d3d80a
sberkaye <berkayergin99@gmail.com>
2020-10-29 16:29:29 +0300

    using JS variables in JSX

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function getButtonText() {
+  return "Click Me!";
+}
 
 function App() {
+  const text = "Hello!";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {getButtonText()}
+      </button>
+      {text}
     </div>
   );
 }
