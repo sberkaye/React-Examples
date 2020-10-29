69e0f4ac2bd809d1b91f5ffbe0ca33c8693a8e9b
sberkaye <berkayergin99@gmail.com>
2020-10-29 19:45:55 +0300

    Initial components commit with a blog comment

---
+++ components/src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            John
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 5:30PM</span>
+          </div>
+          <div className="text">Nice job!</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
