41b42030c8e6bd140c06a10f5ecd12375028827a
sberkaye <berkayergin99@gmail.com>
2020-10-29 15:45:42 +0300

    example#2

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hello world
+          Edit <code>src/App.js</code> and save to reload. Hello world merhaba dünya
         </p>
         <a
           className="App-link"
