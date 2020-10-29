f583e765706a7ee911e447b3879279922492d7d9
sberkaye <berkayergin99@gmail.com>
2020-10-29 19:55:16 +0300

    Coments with naive approach

--- components/src/App.js
+++ components/src/App.js
@@ -1,12 +1,41 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Faker from "faker";
 
 function App() {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" />
+          <img alt="avatar" src={Faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            John
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 5:30PM</span>
+          </div>
+          <div className="text">Nice job!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={Faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            John
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 5:30PM</span>
+          </div>
+          <div className="text">Nice job!</div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={Faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
