233d80e199a564a772519fba0d13690cf597df57
sberkaye <berkayergin99@gmail.com>
2020-11-02 00:48:33 +0300

    Displaying an error message & conditional rendering

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -5,16 +5,24 @@ class App extends React.Component {
     super(props);
 
     // this is the ONLY time the state should be assigned directly
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => console.log(err)
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-    return <div> Location: {this.state.lat}</div>;
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
