b64bdc55a9d83f5a1efafa350b03c9fb039b6c28
sberkaye <berkayergin99@gmail.com>
2020-11-06 00:44:12 +0300

    Further styling the list

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./VideoItem.css";
 
 class VideoItem extends React.Component {
   render() {
     const { video } = this.props;
     return (
-      <div className="item">
+      <div className="item video-item">
         <img className="ui image" src={video.snippet.thumbnails.medium.url} />
         <div className="content">
           <div className="header">{video.snippet.title}</div>
