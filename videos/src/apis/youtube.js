67561f7cc68df368111c27f79a08159d73ef0573
sberkaye <berkayergin99@gmail.com>
2020-11-05 22:52:40 +0300

    Installed axios and created a preconfigured axios instance to use YouTube API

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const API_KEY = "AIzaSyB54YjR-R3_ZNh2PgIymM00ztU7ULPuhu8";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: API_KEY,
+  },
+});
