d1a945b482c965dd6ab2ed741a0cc3a91cdb23c6
sberkaye <berkayergin99@gmail.com>
2020-11-06 17:47:11 +0300

    Creating a helper function inside a function based component

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const handleTitleClick = (index) => {};
+  const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div className="title active" onClick={() => handleTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
