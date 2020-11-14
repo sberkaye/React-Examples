1e688b1ceaaddab5b243e2740d86b7da4387eb2e
sberkaye <berkayergin99@gmail.com>
2020-11-14 21:56:30 +0300

    wiring up an action creator

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,12 +1,5 @@
 import { combineReducers } from "redux";
 
-const reducerOne = (state = [], action) => {
-  if (action.type === "EXAMPLE") {
-    return [...state, action.payload];
-  }
-  return state;
-};
-
 export default combineReducers({
-  reducerOne,
+  dummyReducer: () => 1999,
 });
