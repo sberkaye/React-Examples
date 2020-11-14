4d5ee4c7f00c062af4707881b5cbd864281dd410
sberkaye <berkayergin99@gmail.com>
2020-11-14 22:56:18 +0300

    wiring up redux thunk as a middleware and making async requests by using it

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,7 @@
-export const fetchPosts = () => {
-  return {
-    type: "FETCH_POSTS",
-  };
+import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceHolder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
