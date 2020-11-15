016c585c8d90bb06f6d64007ff51ec518e40ffac
sberkaye <berkayergin99@gmail.com>
2020-11-15 16:42:14 +0300

    Added an event listener to handle the state whenever the user signs in or signs out and created a new helper function to render sign in and sign out buttons

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -4,17 +4,58 @@ const CLIENT_ID =
   "1039097660264-7adirsvjkn234a4a7gl5jcugrcfplh30.apps.googleusercontent.com";
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
   // Initialize Google API library with OAuth client ID
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
       // this callback will be called after Google API finishes loading the required library
-      window.gapi.client.init({
-        clientId: CLIENT_ID,
-      });
+      window.gapi.client
+        .init({
+          clientId: CLIENT_ID,
+          scope: "email",
+        })
+        .then(() => {
+          // After everything is ready, connect the auth instance to this component
+          this.auth = window.gapi.auth2.getAuthInstance();
+          /*
+           *  When this method runs, the component has already been mounted and therefore
+           *  rendered. So we need to get the component to re-render to be able to show
+           *  if the user is logged in or not. We're going to be using component level state
+           *  to achieve re-rendering. When the state is uupdated via this.setState(), the
+           *  component will re-render and show whether the user is signed-in or not.
+           */
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          // Set an event listener to handle the situation whenever auth.isSignedIn changes
+          this.auth.isSignedIn.listen(this.handleAuthChange);
+        });
     });
   }
+
+  handleAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button className="ui blue google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
+    }
+  }
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
