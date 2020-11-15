4fcd4fecf42c84c1bc5f7d3b334d50a6feaf9fec
sberkaye <berkayergin99@gmail.com>
2020-11-16 00:44:42 +0300

    Displaying error messages to the user

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -2,36 +2,37 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
-  renderInput({ input, label, meta }) {
-    /*
-    return (
-      <input
-        value={formProps.input.value}
-        onChange={formProps.input.onChange}
-      />
-    );
-    */
+  renderError = ({ touched, error }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+  renderInput = ({ input, label, meta }) => {
     return (
       <div className="field">
         <label>{label}</label>
-        <input {...input} />
-        {meta.error}
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
   handleFormSubmit = (formValues) => {
     console.log(formValues);
   };
 
   render() {
-    // If Label components sees a prop it doesn't know(label in the current case), it will
+    // If a Field component sees a prop it doesn't know(label in the current case), it will
     // just add the new prop to the end of the props object it passes as an argument
-    // to the function in component prop
+    // to the function in component prop.
     return (
       <form
         onSubmit={this.props.handleSubmit(this.handleFormSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
@@ -59,7 +60,15 @@ const validate = (formValues) => {
   return errors;
 };
 
+// This function is used to bind user interactions to dispatches of Redux actions
+// which means; whenever a user interacts with input components that are located in
+// Field components, they will dispatch redux actions to update the piece of state
+// that is named with the "form" property of the config object. The config object also
+// provides validation method for the form inputs before updating the state with them.
 export default reduxForm({
+  // the name of your form and the key to where your form's state will be mounted
+  // under the redux-form reducer
   form: "streamCreate",
+  // validation method
   validate,
 })(StreamCreate);
