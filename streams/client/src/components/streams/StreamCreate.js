667ad159f013073efa54c26c67305bc9e27a8807
sberkaye <berkayergin99@gmail.com>
2020-11-15 20:37:33 +0300

    Handling form submissions with Redux Form

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
-  renderInput(formProps) {
+  renderInput({ input, label }) {
     /*
     return (
       <input
@@ -11,13 +11,34 @@ class StreamCreate extends Component {
       />
     );
     */
-    return <input {...formProps.input} />; // SAME AS ABOVE - OR SIMILAR
-  }
-  render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
+
+  handleFormSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
+  render() {
+    // If Label components sees a prop it doesn't know(label in the current case), it will
+    // just add the new prop to the end of the props object it passes as an argument
+    // to the function in component prop
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.handleFormSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
