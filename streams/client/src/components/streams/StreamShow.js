8909b8c0dd9f36ef1e674f0921b0a322255f115d
sberkaye <berkayergin99@gmail.com>
2020-11-18 03:24:13 +0300

    Broadcasting the stream with flv.js

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,28 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { connect } from "react-redux";
+import flv from "flv.js";
 
 import { fetchStream } from "../../actions";
 
-const StreamShow = (props) => {
-  const { id } = props.match.params;
-  // Component isolation
-  useEffect(() => {
-    props.fetchStream(id);
-  }, [props, id]);
-
-  if (!props.stream) {
-    return <div>Loading...</div>;
+class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
 
-  const { title, description } = props.stream;
+  // Component isolation
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
 
-  return (
-    <div>
-      <h1>{title}</h1>
-      <h5>{description}</h5>
-    </div>
-  );
-};
+  // if the player can not be built in the initial render after componentDidMount
+  // try to build it again with each re-render
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  // build the flv player with the correct id and load it
+  buildPlayer = () => {
+    // if we don't have the stream or the player has already been built, return
+    if (!this.props.stream || this.flvPlayer) {
+      return;
+    }
+    const { id } = this.props.match.params;
+    this.flvPlayer = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.flvPlayer.attachMediaElement(this.videoRef.current);
+    this.flvPlayer.load();
+  };
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    const { title, description } = this.props.stream;
+    return (
+      <div>
+        <video ref={this.videoRef} style={{ width: "80%" }} controls />
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
