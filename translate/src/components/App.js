82e751aae9d389bf1c062101a39e3b6ffe7478b4
sberkaye <berkayergin99@gmail.com>
2020-11-18 15:46:14 +0300

    Scaffolding the app and styling

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
 
+import UserCreate from "./UserCreate";
+
 class App extends Component {
+  state = { language: "english" };
+  handleLanguageChange = (language) => {
+    this.setState({ language });
+  };
   render() {
-    return <div className="ui container">SAsaas</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag uk"
+            onClick={() => this.handleLanguageChange("english")}
+          />
+          <i
+            className="flag tr"
+            onClick={() => this.handleLanguageChange("turkish")}
+          />
+        </div>
+        <UserCreate />
+      </div>
+    );
   }
 }
 
